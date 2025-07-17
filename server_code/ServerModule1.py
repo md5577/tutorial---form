3dc10a095d6148ea7b44a3a5ffc0413eefa8fb1a
marius@point.co <marius@point.co>
2025-07-17 21:26:28 +0000

    Edited settings and ServerModule1

--- server_code/ServerModule1.py
+++ server_code/ServerModule1.py
@@ -1,3 +1,4 @@
+import anvil.email
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
@@ -19,10 +20,22 @@ from datetime import datetime
 
 @anvil.server.callable
 def add_feedback(name, email, feedback):
+  
   app_tables.feedback.add_row(
     name=name, 
     email=email, 
     feedback=feedback, 
     created=datetime.now()
   )
+  
+  # Send yourself an email each time feedback is submitted
+  anvil.email.send(#to="noreply@anvil.works", # Change this to your email address!
+    subject=f"Feedback from {name}",
+    text=f"""
+  A new person has filled out the feedback form!
 
+  Name: {name}
+  Email address: {email}
+  Feedback:
+  {feedback}
+  """)
